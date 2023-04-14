6b907646ea570448e76cc18c46f14b77e8daf48a
avi2129934 <avi2129934@gmail.com>
2023-04-14 14:48:44 +0530

    Deployed Website

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import {HashRouter as Router, Routes, Route} from 'react-router-dom';
 import Home from './pages/Home'
 import About from './pages/About'
 import Search from './pages/Search'
@@ -8,7 +8,7 @@ import Explore from './pages/Explore'
 
 function App() {
   return (
-    <>
+    <div>
       <Router>
         <Routes>
           <Route path='/' element={<Layout/>}>
@@ -19,7 +19,7 @@ function App() {
           </Route>
         </Routes>
       </Router>
-    </>
+    </div>
   );
 }
 
